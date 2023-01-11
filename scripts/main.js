93df1c6c9672fcf8aac3fbc8c10221fa3dfcd619
OBM <mesilaned@gmail.com>
2023-01-11 19:20:34 +0300

    Git init

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".i__search").click(function() {
+        $(".form__serach, .input__serach").toggleClass("active");
+        $("input[type='text']").focus;
+    });
+});
